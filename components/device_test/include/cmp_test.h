f5400f3060153b0362ae402bb32740e615121c14
yuanym <yuanye.min@163.com>
2023-03-30 13:40:57 +0800

    模块测试，测试头文件分开处理

---
+++ components/device_test/include/cmp_test.h
@@ -0,0 +1,22 @@
+/***********************************************************************//** 
+ * \file  cmp test.h
+ * \brief  cmp test description and static inline functions at register level 
+ * \copyright Copyright (C) 2015-2020 @ APTCHIP
+ * <table>
+ * <tr><th> Date  <th>Version  <th>Author  <th>Description
+ * <tr><td> 2023-3-24 <td>V0.0  <td>YYM   <td>initial
+ * </table>
+ * *********************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _CMP_TEST_H
+#define _CMP_TEST_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "common.h"
+
+//cmp test
+
+
+#endif
--- components/device_test/include/syscon_test.h
+++ components/device_test/include/syscon_test.h
@@ -1,5 +1,5 @@
 /***********************************************************************//** 
- * \file  test.h
+ * \file  syscon test.h
  * \brief  test description and static inline functions at register level 
  * \copyright Copyright (C) 2015-2020 @ APTCHIP
  * <table>
@@ -10,19 +10,13 @@
 */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef _TEST_H
-#define _TEST_H
+#ifndef _SYSCON_TEST_H
+#define _SYSCON_TEST_H
 
 /* Includes ------------------------------------------------------------------*/
 #include "common.h"
 
-//gpta test
+//syscon test
 
-extern int gpta_timer_test(void);
-extern int gpta_capture_sync_test(void);
-extern int gpta_capture_sync_test1(void);
-extern int gpta_pwm_test(void);
-extern int gpta_pwm_waveform_test(void);
-extern int gpta_reglk_test(void);
 
 #endif
