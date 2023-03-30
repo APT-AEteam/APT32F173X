6f48011019ce615a97b5b17c9944d3ef97f00906
Ning <2211484@qq.com>
2023-03-30 13:41:03 +0800

    新增ifc_test.c和.h

---
+++ components/device_test/include/ifc_test.h
@@ -0,0 +1,7 @@
+
+csi_error_t eflash_fullrange_test(void);
+csi_error_t eflash_parallel_test(void);
+void eflash_useroption_extrst_test(void);
+void eflash_useroption_iwdt_test(void);
+void eflash_hdp_test(void);
+void eflash_endurance(void);
