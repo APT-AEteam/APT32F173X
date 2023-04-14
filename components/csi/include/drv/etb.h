bb3cf9ae59d7e232c4090229f0dd728ffe65069d
yangry111 <405916987@qq.com>
2023-04-14 10:43:49 +0800

    173 删除ETCB多对一模式

--- components/csi/include/drv/etb.h
+++ components/csi/include/drv/etb.h
@@ -34,8 +34,7 @@ typedef enum {
  */
 typedef enum {
     ETB_ONE_TRG_ONE	= 0,      //one device trig one deivce, 3~31channel
-    ETB_ONE_TRG_MORE,         //one device trig two for more device, 1~2channel
-    ETB_MORE_TRG_ONE,         //two or more device trig one deivce, 0 channel
+    ETB_ONE_TRG_MORE,         //one device trig two for more device, 0~2channel
 	ETB_ONE_TRG_ONE_DMA       //one device trig one deivce of dma, 20->31channel
 } csi_etb_ch_type_e;
 
