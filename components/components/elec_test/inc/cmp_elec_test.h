8f8d07c913cad2dd2224906584a6180a0e6afbf8
yuanym <yuanye.min@163.com>
2023-04-14 10:27:59 +0800

    添加cmp和osc电气参数测试工程

---
+++ components/components/elec_test/inc/cmp_elec_test.h
@@ -0,0 +1,20 @@
+/***********************************************************************//** 
+ * \file  cmp_elec_test.h
+ * \brief  cmp电气参数测试代码
+ * \copyright Copyright (C) 2015-2020 @ APTCHIP
+ * <table>
+ * <tr><th> Date  <th>Version  <th>Author  <th>Description
+ * <tr><td> 2023-4-6 <td>V0.0  <td>YYM   <td>initial
+ * </table>
+ * *********************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _CMP_ELEC_TEST_H
+#define _CMP_ELEC_TEST_H
+
+extern int cmp0_offset_voltage_test(void);
+extern int cmp1_offset_voltage_test(void);
+extern int cmp2_offset_voltage_test(void);
+
+#endif
