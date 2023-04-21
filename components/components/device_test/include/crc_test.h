4eaf13327f0e2752bd4082c89c5dc89f89456912
yangry111 <405916987@qq.com>
2023-04-21 14:36:06 +0800

    173 etcb/crc/uart/usart/rtc 组件更新

---
+++ components/components/device_test/include/crc_test.h
@@ -0,0 +1,21 @@
+/***********************************************************************//** 
+ * \file  crc test.h
+ * \brief  crc test description and static inline functions at register level 
+ * \copyright Copyright (C) 2015-2020 @ APTCHIP
+ * <table>
+ * <tr><th> Date  <th>Version  <th>Author  <th>Description
+ * <tr><td> 2023-4-10 <td>V0.0  <td>CWL   <td>initial
+ * </table>
+ * *********************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _CRC_TEST_H
+#define _CRC_TEST_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "common.h"
+
+extern void crc_demo_test(void);
+
+#endif
---
+++ components/components/device_test/include/lin_test.h
@@ -0,0 +1,23 @@
+/***********************************************************************//** 
+ * \file  lin test.h
+ * \brief  lin test description and static inline functions at register level 
+ * \copyright Copyright (C) 2015-2020 @ APTCHIP
+ * <table>
+ * <tr><th> Date  <th>Version  <th>Author  <th>Description
+ * <tr><td> 2023-4-10 <td>V0.0  <td>CWL   <td>initial
+ * </table>
+ * *********************************************************************
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _LIN_TEST_H
+#define _LIN_TEST_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "common.h"
+
+
+extern int lin_send_recv_test(void);
+extern int lin_send_test(void);
+
+#endif
