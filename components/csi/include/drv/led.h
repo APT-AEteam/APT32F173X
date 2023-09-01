9dc2d173fe434f09f11d287a52e40a26fc540891
wangch <Will.wch@aptchip.com>
2023-09-01 17:58:00 +0800

    更新led驱动： 1、完善demo，删除不需要的代码 2、修改pin接口为gpio 3、函数接口增加apt开头 4、去掉结构体中的中断变量

--- components/csi/include/drv/led.h
+++ components/csi/include/drv/led.h
@@ -4,7 +4,9 @@
  * \copyright Copyright (C) 2015-2020 @ APTCHIP
  * <table>
  * <tr><th> Date  <th>Version  <th>Author  <th>Description
- * <tr><td> 2020-12-8 <td>V0.0  <td>WNN   <td>initial
+ * <tr><td> 2020-12-8  <td>V0.0  <td>WNN   <td>initial
+ * <tr><td> 2023-3-21  <td>V0.1  <td>WCH   <td>modified
+ * <tr><td> 2023-9-1   <td>V0.2  <td>WCH   <td>modified
  * </table>
  * *********************************************************************
 */
@@ -12,8 +14,6 @@
 #ifndef _LED_H_
 #define _LED_H_
 
-#include "stdint.h"
-#include "common.h"
 #include "csp.h"
 
 #ifdef __cplusplus
@@ -53,41 +53,50 @@ typedef enum {
 }csi_led_intsrc_e;
 
 typedef struct csi_led_config {
-	uint8_t 	byClk;			//clk configure
+	uint8_t 	byClkDiv;			//clk configure
 	uint8_t 	byBrt;			//brightness configure
 	uint16_t 	hwComMask;		//COM enable
-	uint16_t 	byOnTime;		//scanning timing: COM on cycles(range:56~2096).Tcom = byOnTime * Tledclk, needs to be a multiple of 8, otherwise the timing will NOT be accurate
-	uint16_t 	byBreakTime;	//scanning timing: cycles between COMs(range:14~524).Tbreak = byBreakTime * Tledclk.
-	uint8_t 	byInt;			//Interrupt Source
+	uint16_t 	hwOnTime;		//scanning timing: COM on cycles(range:56~2096).Tcom = byOnTime * Tledclk, needs to be a multiple of 8, otherwise the timing will NOT be accurate
+	uint16_t 	hwBreakTime;	//scanning timing: cycles between COMs(range:14~524).Tbreak = byBreakTime * Tledclk.
 }csi_led_config_t;
 
 
-/**
-  \brief   	   led init & configuration: clk, brightness, COM mask
-  \param[in]   ptLedBase    pointer of LED register structure
-  \param[in]   eClk		IP clock of LED-Fled. LED_PCLK_DIV4/8/16/32/64/128/256
-  \param[in]   ebrt 	LED brightness: LED_100/87/75/62/50/37/25/12
-  \param[in]   byComMask	to select which COMs to be light on
-  \return     csi_error_t  
-*/
+ /** \brief initialize uart parameter structure
+ * 
+ *  \param[in] ptLedBase: pointer of led register structure
+ *  \param[in] ptLedCfg: pointer of led parameter config structure
+ * 			   - byClkDiv: LED Clock divider
+ * 			   - byBrt: LED brightness control
+ * 			   - hwComMask: COM port enable mask
+ * 			   - hwOnTime: scanning timing: COM on cycles(range:56~2096).Tcom = byOnTime * Tledclk, needs to be a multiple of 8, otherwise the timing will NOT be accurate
+ * 			   - hwBreakTime: scanning timing: cycles between COMs(range:14~524).Tbreak = byBreakTime * Tledclk.
+ *  \return error code \ref csi_error_t
+ */
 csi_error_t csi_led_init(csp_led_t *ptLedBase, csi_led_config_t *tLedCfg);
 
 /** \brief   led bright control
  * 
  *  \param[in] ptLedBase: pointer of LED register structure
- *  \param[in] eBrt:	LED bright control,refer to csi_led_brt_e
+ *  \param[in] eBrt:	LED bright control \ref csi_led_brt_e
  *  \return  none  
  */
 void csi_led_set_bright(csp_led_t *ptLedBase, csi_led_brt_e eBrt);
 
-/** 
-  \brief 	   LED interrupt enable/disable control
-  \param[in]   ptLedBase	pointer of bt register structure
-  \param[in]   eIntSrc		bt interrupt source
-  \param[in]   bEnable		enable/disable interrupt
-  \return none
+/** \brief LED interrupt enable control
+ * 
+ *  \param[in] ptLedBase: pointer of bt register structure
+ *  \param[in] eIntSrc: led interrupt source \ref csi_led_intsrc_e
+ *  \return none
+ */
+void csi_led_int_enable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc);
+
+/** \brief LED interrupt disable control
+ * 
+ *  \param[in] ptLedBase: pointer of bt register structure
+ *  \param[in] eIntSrc: led interrupt source \ref csi_led_intsrc_e
+ *  \return none
  */ 
-void csi_led_int_enable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc, bool bEnable);
+void csi_led_int_disable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc);
  
 /**
   \brief   	   write led data
@@ -120,14 +129,14 @@ void csi_led_blink_control(csp_led_t *ptLedBase, csi_led_blk_e eLedBlk, uint16_t
   \param[in]   ptLedBase    pointer of LED register structure
   \return  	   None
 */
-void csi_led_lighton(csp_led_t *ptLedBase);
+void csi_led_light_on(csp_led_t *ptLedBase);
 
 /**
   \brief   	   led stop
   \param[in]   ptLedBase    pointer of LED register structure
   \return  	   None
 */
-void csi_led_lightoff(csp_led_t *ptLedBase);
+void csi_led_light_off(csp_led_t *ptLedBase);
 
 
 
