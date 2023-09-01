7f34d9449c35586c368b275015dcd37c78a769cf
wangch <Will.wch@aptchip.com>
2023-09-01 17:55:55 +0800

    更新LED驱动： 1、csp文件中inline改为define 2、增加int_disable接口 3、完善注释

--- components/chip/drivers/led.c
+++ components/chip/drivers/led.c
@@ -5,46 +5,44 @@
  * <table>
  * <tr><th> Date  <th>Version  <th>Author  <th>Description
  * <tr><td> 2020-12-8 <td>V0.0  <td>WNN   <td>initial
+ * <tr><td> 2023-3-21 <td>V0.1  <td>WCH  <td>modified
+ * <tr><td> 2023-9-1  <td>V0.2  <td>WCH  <td>modified
  * </table>
  * *********************************************************************
 */
-#include "csp.h"
-#include "common.h"
-#include "csp_led.h"
-#include "led.h"
-#include "irq.h"
-#include "csp_common.h"
-#include "sys_clk.h"
+#include <drv/led.h>
 
-/** \brief   led init & configuration: clk, brightness, COM mask
+ /** \brief initialize uart parameter structure
  * 
- *  \param[in] ptLedBase: pointer of LED register structure
- *  \param[in] ptLedCfg:	pointer of led parameter config structure
- *  \return  csi_error_t  
- */
+ *  \param[in] ptLedBase: pointer of led register structure
+ *  \param[in] ptLedCfg: pointer of led parameter config structure
+ * 			   - byClkDiv: LED Clock divider
+ * 			   - byBrt: LED brightness control
+ * 			   - hwComMask: COM port enable mask
+ * 			   - hwOnTime: scanning timing: COM on cycles(range:56~2096).Tcom = byOnTime * Tledclk, needs to be a multiple of 8, otherwise the timing will NOT be accurate
+ * 			   - hwBreakTime: scanning timing: cycles between COMs(range:14~524).Tbreak = byBreakTime * Tledclk.
+ *  \return error code \ref csi_error_t
+ */ 
 csi_error_t csi_led_init(csp_led_t *ptLedBase, csi_led_config_t *ptLedCfg) 
 {
 	csi_clk_enable((uint32_t *)ptLedBase);	
 	csi_led_set_bright(ptLedBase, ptLedCfg->byBrt);
-	csp_led_set_clk(ptLedBase, ptLedCfg->byClk);
-	csp_led_set_commask(ptLedBase, (ptLedCfg->hwComMask) & 0x3ff);
-	if(ptLedCfg->byOnTime > 2096 || ptLedCfg->byBreakTime > 524)
+	csp_led_set_clk(ptLedBase, ptLedCfg->byClkDiv);
+	csp_led_set_commask(ptLedBase, (ptLedCfg->hwComMask & 0x3ff));
+	if(ptLedCfg->hwOnTime > 2096 || ptLedCfg->hwBreakTime > 524)
 		return CSI_ERROR;
-	csp_led_set_dcomcnt(ptLedBase, (ptLedCfg->byOnTime/8 - 7));
-	csp_led_set_novcnt(ptLedBase, (ptLedCfg->byBreakTime/2 -7));
+	csp_led_set_dcomcnt(ptLedBase, (ptLedCfg->hwOnTime/8 - 7));
+	csp_led_set_novcnt(ptLedBase, (ptLedCfg->hwBreakTime/2 -7));
 	
-	if(ptLedCfg->byInt) {
-		csp_led_int_enable(ptLedBase, ptLedCfg->byInt, ENABLE);
-		csi_irq_enable((uint32_t *)ptLedBase);
-	}
+	csi_irq_enable((uint32_t *)ptLedBase);
+	
 	return CSI_OK;
-	
 }
 
 /** \brief   led bright control
  * 
  *  \param[in] ptLedBase: pointer of LED register structure
- *  \param[in] eBrt:	LED bright control,refer to csi_led_brt_e
+ *  \param[in] eBrt:	LED bright control \ref csi_led_brt_e
  *  \return  none  
  */
 void csi_led_set_bright(csp_led_t *ptLedBase, csi_led_brt_e eBrt) 
@@ -52,26 +50,28 @@ void csi_led_set_bright(csp_led_t *ptLedBase, csi_led_brt_e eBrt)
 	csp_led_set_brt(ptLedBase,(csp_led_brt_e)eBrt);
 }
 
-/** \brief LED interrupt enable/disable control
+/** \brief LED interrupt enable control
  * 
  *  \param[in] ptLedBase: pointer of bt register structure
- *  \param[in] eIntSrc: led interrupt source
- *  \param[in] bEnable: enable/disable interrupt
+ *  \param[in] eIntSrc: led interrupt source \ref csi_led_intsrc_e
  *  \return none
  */ 
-void csi_led_int_enable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc, bool bEnable)
+void csi_led_int_enable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc)
 {
-	csp_led_int_enable(ptLedBase, (csp_led_int_e)eIntSrc, bEnable);
-	
-	if (bEnable) {
-		csi_irq_enable((uint32_t *)ptLedBase);
-	}
-	else {
-		if (eIntSrc == csp_led_get_misr(ptLedBase)) {
-			csi_irq_disable((uint32_t *)ptLedBase);
-		}
-	}
+	csp_led_int_enable(ptLedBase, (csp_led_int_e)eIntSrc);
 }
+
+/** \brief LED interrupt disable control
+ * 
+ *  \param[in] ptLedBase: pointer of bt register structure
+ *  \param[in] eIntSrc: led interrupt source \ref csi_led_intsrc_e
+ *  \return none
+ */ 
+void csi_led_int_disable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc)
+{
+	csp_led_int_disable(ptLedBase, (csp_led_int_e)eIntSrc);
+}
+
 /** \brief   write led data
  * 
  *  \param[in] ptLedBase: pointer of LED register structure
@@ -82,7 +82,6 @@ void csi_led_int_enable(csp_led_t *ptLedBase, csi_led_intsrc_e eIntSrc, bool bEn
 void csi_led_write_data(csp_led_t *ptLedBase, uint8_t byCom, uint8_t byData)
 {
 	csp_led_set_setdata(ptLedBase, byCom, byData);
-	
 }
 
 /** \brief   set led blink pattern
@@ -100,7 +99,7 @@ void csi_led_set_blink_pattern(csp_led_t *ptLedBase, uint16_t hwOnMsk)
 /** \brief  led blink control
  * 
  * \param[in] ptLedBase: pointer of LED register structure
- * \param[in] eLedBlk: blink on or blink off
+ * \param[in] eLedBlk: blink on or blink off \ref csi_led_intsrc_e
  * \param[in] hwOnMsk: on pattern
  * \return  None
  */
@@ -111,21 +110,21 @@ void csi_led_blink_control(csp_led_t *ptLedBase, csi_led_blk_e eLedBlk, uint16_t
 	else if(eLedBlk == LED_BLK_OFF)
 		csp_led_com_off(ptLedBase, (hwOnMsk & LED_BLK_MSK));
 }
-/** \brief   led start
+/** \brief   led scan start
  * 
  * \param[in] ptLedBase: pointer of LED register structure
  * \return None
  */
-void csi_led_lighton(csp_led_t *ptLedBase)
+void csi_led_light_on(csp_led_t *ptLedBase)
 {
 	csp_led_light_on(ptLedBase);
 }
-/** \brief led stop
+/** \brief led scan stop
  * 
  *  \param[in] ptLedBase: pointer of LED register structure
  *  \return None
  */
-void csi_led_lightoff(csp_led_t *ptLedBase)
+void csi_led_light_off(csp_led_t *ptLedBase)
 {	
 	csp_led_light_off(ptLedBase);
 }
--- components/chip/include/csp_led.h
+++ components/chip/include/csp_led.h
@@ -5,7 +5,9 @@
  * <table>
  * <tr><th> Date  <th>Version  <th>Author  <th>Description
  * <tr><td> 2020-8-20 <td>V0.0  <td>ZJY   <td>initial
- * <tr><td> 2020-12-3 <td>V0.1  <td>WNN  <td>modiied
+ * <tr><td> 2020-12-3 <td>V0.1  <td>WNN   <td>modiied
+ * <tr><td> 2023-3-21 <td>V0.1  <td>WCH   <td>modified
+ * <tr><td> 2023-9-1  <td>V0.2  <td>WCH   <td>modified
  * 
  * </table>
  * *********************************************************************
@@ -22,31 +24,30 @@
 /// \brief LED Reg Description
 typedef volatile struct
 {
-    __IOM uint32_t      CR;				//0x0000	Control Register                 
-    __IOM uint32_t      BRIGHT;       	//0x0004   	Brightness Control Register      
-    __IM  uint32_t      RISR;         	//0x0008   	Raw Interrupt Status Register    
-    __IOM uint32_t      IMCR;         	//0x000C   	Interrupt Masking Control Reg.   
-    __IM  uint32_t      MISR;         	//0x0010    Masked Interrupt Status Register 
-    __OM  uint32_t      ICR;          	//0x0014   	Interrupt Clear Register         
-    __IOM uint32_t      RSVD;			//0x0018 
-    __IOM uint32_t      TIMCR;        	//0x001C    Timing Control Register          
-    __OM  uint32_t      BLKER;       	//0x0020    Blink Enable Control Register    
-    __OM  uint32_t      BLKDR;        	//0x0024   	Blink Disable Control Register   
-    __IM  uint32_t      BLKST;        	//0x0028    Blink Status Control Register    
-    __IOM uint32_t      SEGDATA[10];    //0x002C~0x0050    Segment Data                   
-                   
+    __IOM uint32_t      CR;				//0x0000		Control Register                 
+    __IOM uint32_t      BRIGHT;       	//0x0004   		Brightness Control Register      
+    __IM  uint32_t      RISR;         	//0x0008   		Raw Interrupt Status Register    
+    __IOM uint32_t      IMCR;         	//0x000C   		Interrupt Masking Control Reg.   
+    __IM  uint32_t      MISR;         	//0x0010    	Masked Interrupt Status Register 
+    __OM  uint32_t      ICR;          	//0x0014   		Interrupt Clear Register         
+    __IOM uint32_t      RSVD;			//0x0018 		Reserved
+    __IOM uint32_t      TIMCR;        	//0x001C    	Timing Control Register          
+    __OM  uint32_t      BLKER;       	//0x0020    	Blink Enable Control Register    
+    __OM  uint32_t      BLKDR;        	//0x0024   		Blink Disable Control Register   
+    __IM  uint32_t      BLKST;        	//0x0028    	Blink Status Control Register    
+    __IOM uint32_t      SEGDATA[10];    //0x002C~0x0050 Segment Data
  } csp_led_t;
  
 /******************************************************************************
 * LED Regiser
 ******************************************************************************/ 
 ///CR
-#define LED_COM_EN_POS                  16                                             /*!< LED CR: COM_EN Position */
-#define LED_COM_EN_MSK                  (0x3FFUL << LED_COM_EN_POS)                 /*!< LED CR: COM_EN Mask */
+#define LED_COM_EN_POS                  16                                     /*!< LED CR: COM_EN Position */
+#define LED_COM_EN_MSK                  (0x3FFUL << LED_COM_EN_POS)            /*!< LED CR: COM_EN Mask */
 
-#define LED_LEDCLK_POS                  1                                              /*!< LED CR: LEDCLK Position */
-#define LED_LEDCLK_MSK                  (0x7UL << LED_LEDCLK_POS)                   /*!< LED CR: LEDCLK Mask */
-typedef enum {
+#define LED_LEDCLK_POS                  1                                      /*!< LED CR: LEDCLK Position */
+#define LED_LEDCLK_MSK                  (0x7UL << LED_LEDCLK_POS)              /*!< LED CR: LEDCLK Mask */
+typedef enum{
 	LED_PCLK_D4 = 0,
 	LED_PCLK_D8,
 	LED_PCLK_D16,
@@ -56,12 +57,11 @@ typedef enum {
 	LED_PCLK_D256,
 }csp_led_ledclk_e;
 
-#define LED_LIGHTON_POS                 0                                              /*!< LED CR: LIGHTON Position */
-#define LED_LIGHTON_MSK                 (0x1UL << LED_LIGHTON_POS)                  /*!< LED CR: LIGHTON Mask */
-
+#define LED_LIGHTON_POS                 0                                       /*!< LED CR: LIGHTON Position */
+#define LED_LIGHTON_MSK                 (0x1UL << LED_LIGHTON_POS)              /*!< LED CR: LIGHTON Mask */
 
 ///BRIGHT
-#define LED_BRT_POS                 	0                                              /*!< LED BRIGHT: BRT Position */
+#define LED_BRT_POS                 	0                                       /*!< LED BRIGHT: BRT Position */
 #define LED_BRT_MSK                 	(0x7UL << LED_BRT_POS)                  /*!< LED BRIGHT: BRT Mask */
 typedef enum {
 	COM100 = 0,
@@ -81,85 +81,44 @@ typedef enum {
 }csp_led_int_e;
 
 ///TIMCR
-#define LED_NOVCNT_POS               	8                                              /*!< LED TIMCR: NOVCNT Position */
+#define LED_NOVCNT_POS               	8                                        /*!< LED TIMCR: NOVCNT Position */
 #define LED_NOVCNT_MSK               	(0xFFUL << LED_NOVCNT_POS)               /*!< LED TIMCR: NOVCNT Mask */
 
-#define LED_DCOMCNT_POS              	0                                              /*!< LED TIMCR: DCOMCNT Position */
+#define LED_DCOMCNT_POS              	0                                        /*!< LED TIMCR: DCOMCNT Position */
 #define LED_DCOMCNT_MSK              	(0xFFUL << LED_DCOMCNT_POS)              /*!< LED TIMCR: DCOMCNT Mask */
  
- ///BLKER/BLKDR/BLKST
+///BLKER/BLKDR/BLKST
 #define LED_BLK_MSK						(0x3ff)
 
 ///SEGDATA0~9
-#define LED_SEGDATA_POS(n)              n                                              /*!< LED SEGDATn: Position */
-#define LED_SEGDATA_MSK(n)              (0x1UL << LED_SEGDAT0_SEG7_POS)                /*!< LED SEGDATn: Mask */
+#define LED_SEGDATA_POS(n)              n                                        /*!< LED SEGDATn: Position */
+#define LED_SEGDATA_MSK(n)              (0x1UL << LED_SEGDAT0_SEG7_POS)          /*!< LED SEGDATn: Mask */
 
-/*****************************************************************************
- * static inline functions
- ****************************************************************************/
-static inline void csp_led_light_on(csp_led_t *ptLedBase) 
-{
-	ptLedBase -> CR = (ptLedBase -> CR & (~LED_LIGHTON_MSK))| (0x1 << LED_LIGHTON_POS);
-}
-static inline void csp_led_light_off(csp_led_t *ptLedBase) 
-{
-	ptLedBase -> CR = (ptLedBase -> CR & (~LED_LIGHTON_MSK));
-}
-static inline void csp_led_set_clk(csp_led_t *ptLedBase, csp_led_ledclk_e eClk)
-{
-	ptLedBase -> CR = (ptLedBase -> CR &(~LED_LEDCLK_MSK)) | (eClk << LED_LEDCLK_POS);
-}
-static inline void csp_led_set_commask(csp_led_t *ptLedBase, uint16_t byComMsk)
-{
-	ptLedBase -> CR = (ptLedBase -> CR & (~LED_COM_EN_MSK)) | (byComMsk << LED_COM_EN_POS);
-}
-static inline void csp_led_set_brt(csp_led_t *ptLedBase, csp_led_brt_e eBrt)
-{
-	ptLedBase -> BRIGHT = eBrt;
-}
+/******************************************************************************
+********************* LEDx Macro Definition Declaration **********************
+******************************************************************************/
+//on/off
+#define csp_led_light_on(LEDx) 			((LEDx) -> CR = ((LEDx) -> CR & (~LED_LIGHTON_MSK)) | (0x1 << LED_LIGHTON_POS))
+#define csp_led_light_off(LEDx) 		((LEDx) -> CR = (LEDx) -> CR & (~LED_LIGHTON_MSK))
+#define csp_led_com_on(LEDx, hwVal)		((LEDx) -> BLKDR = hwVal)
+#define csp_led_com_off(LEDx,hwVal)		((LEDx) -> BLKER = hwVal)
 
-static inline void csp_led_int_enable(csp_led_t *ptLedBase, csp_led_int_e eInt, bool bEnable)
-{
-	if(bEnable)
-		ptLedBase->IMCR |= eInt; 
-	else
-		ptLedBase->IMCR &= ~eInt; 
-}
-static inline void csp_led_clr_int(csp_led_t *ptLedBase, csp_led_int_e eInt)
-{
-	ptLedBase -> ICR =  eInt;
-}
-static inline uint32_t csp_led_get_misr(csp_led_t *ptLedBase)
-{
-	return ptLedBase -> MISR;
-}
-static inline uint32_t csp_led_get_risr(csp_led_t *ptLedBase)
-{
-	return ptLedBase -> RISR;
-}
-static inline void csp_led_set_dcomcnt(csp_led_t *ptLedBase, uint8_t byVal)
-{
-	ptLedBase -> TIMCR = (ptLedBase -> TIMCR & (~LED_DCOMCNT_MSK)) | (byVal << LED_DCOMCNT_POS);
-}
-static inline void csp_led_set_novcnt(csp_led_t *ptLedBase, uint8_t byVal)
-{
-	ptLedBase -> TIMCR = (ptLedBase -> TIMCR & (~LED_NOVCNT_MSK)) |(byVal << LED_NOVCNT_POS);
-}
-static inline void csp_led_com_off(csp_led_t *ptLedBase, uint16_t hwDisMsk)
-{
-	ptLedBase -> BLKER = hwDisMsk;
-}
-static inline void csp_led_com_on(csp_led_t *ptLedBase, uint16_t hwClrMsk)
-{
-	ptLedBase -> BLKDR = hwClrMsk;
-}
-static inline uint16_t csp_led_get_com_st(csp_led_t *ptLedBase)
-{
-	return (ptLedBase -> BLKST);
-}
-static inline void csp_led_set_setdata(csp_led_t *ptLedBase, uint8_t byCom, uint8_t byData)
-{
-	ptLedBase -> SEGDATA[byCom] = byData;
-}
+//write reg
+#define csp_led_set_clk(LEDx, eClk) 		((LEDx) -> CR = ((LEDx) -> CR & (~LED_LEDCLK_MSK)) | (eClk << LED_LEDCLK_POS))
+#define csp_led_set_brt(LEDx, eBrt)  	    ((LEDx) -> BRIGHT = eBrt)
+#define csp_led_set_commask(LEDx, byComMsk) ((LEDx) -> CR = ((LEDx) -> CR & (~LED_COM_EN_MSK)) | (byComMsk << LED_COM_EN_POS))
+
+#define csp_led_set_dcomcnt(LEDx, byVal)	((LEDx) -> TIMCR = ((LEDx) -> TIMCR & (~LED_DCOMCNT_MSK)) | (byVal << LED_DCOMCNT_POS))
+#define csp_led_set_novcnt(LEDx, byVal)     ((LEDx) -> TIMCR = ((LEDx) -> TIMCR & (~LED_NOVCNT_MSK)) | (byVal << LED_NOVCNT_POS))
+#define csp_led_set_setdata(LEDx, byCom, byData) ((LEDx) -> SEGDATA[byCom] = byData)
+
+//read reg
+#define csp_led_get_com_st(LEDx)  	((LEDx) -> BLKST)
+
+//interrupt
+#define csp_led_int_enable(LEDx, eInt)	((LEDx) -> IMCR |= eInt)
+#define csp_led_int_disable(LEDx, eInt)	((LEDx) -> IMCR &= ~eInt)
+#define csp_led_clr_int(LEDx, eInt)		((LEDx) -> ICR  =  eInt)
+#define csp_led_get_misr(LEDx)			((LEDx) -> MISR)
 
 #endif
