9c128fc2fc439f7ca033a498da4370a03a82d9d6
Luhydadada <734524706@qq.com>
2023-09-07 17:12:26 +0800

    更新CNTA_IDX， CMP_IDX，  GPTA_IDX 宏定义
    Signed-off-by: Luhydadada <734524706@qq.com>

--- components/chip/drivers/sys/soc.h
+++ components/chip/drivers/sys/soc.h
@@ -62,6 +62,9 @@ extern "C" {
 #define USART_IDX  			2		//USARTx id number
 #define SIO_IDX				2		//UART(0/1)
 #define LED_IDX  			1		//LEDx id number
+#define CNTA_IDX  			1		//CNTAx id number
+#define CMP_IDX  			3		//CMPx id number
+#define GPTA_IDX  			4		//GPTAx id number
 
 //DMA  id number
 //max channel number
