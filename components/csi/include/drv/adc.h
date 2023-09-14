8fbc703e73f750c759ddc54af830ca0991a550a1
Andreas-hsiao <807064338@qq.com>
2023-09-14 11:23:51 +0800

    修复此前造成的ADC.H错误

--- components/csi/include/drv/adc.h
+++ components/csi/include/drv/adc.h
@@ -12,6 +12,10 @@
 #ifndef _DRV_ADC_H_
 #define _DRV_ADC_H_
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <drv/common.h>
+
 #include "csp.h"
 
 #ifdef __cplusplus
@@ -311,28 +315,19 @@ typedef struct {
 	csi_adc_seq_t		*ptSeqCfg;		//pointer of send buf sequence config 
 } csi_adc_config_t;
 
-/// \struct csi_adc_ctrl_t
-/// \brief  adc control handle, not open to users  
-typedef struct 
-{
-    void(*callback)(csp_adc_t *ptAdcBase, uint8_t byIsr);
-} csi_adc_ctrl_t;
 
-/** \brief  register adc interrupt callback function
- * 
- *  \param[in] ptAdcBase: pointer of adc register structure
- *  \param[in] callback: adc interrupt handle function
- *  \return error code \ref csi_error_t
- */ 
-csi_error_t csi_adc_register_callback(csp_adc_t *ptAdcBase, void  *callback);
+/// \struct csi_adc_samp_t
+/// \brief  adc sample handle, not open to users
+typedef struct {
+	uint16_t			*phwData;		//pointer of receive adc data buffer
+	uint16_t			hwChnlDep;		//channel sample depth(data sample length of channel)
+	uint16_t			hwSampCnt;		//channel sample depth count
+	uint8_t				byChnlNum;		//channel number of sequence 
+	uint8_t				byConvStat;		//adc working status
+} csi_adc_samp_t;
 
-/** \brief adc interrupt handler function
- * 
- *  \param[in] ptAdcBase: pointer of adc register structure
- *  \param[in] byIdx: acd idx(0/1)
- *  \return none
- */ 
-void csi_adc_irqhandler(csp_adc_t *ptAdcBase, uint8_t byIdx);
+extern csi_adc_samp_t g_tAdcSamp;
+
 
 /**
   \brief       Initialize adc Interface. Initialize the resources needed for the adc interface
